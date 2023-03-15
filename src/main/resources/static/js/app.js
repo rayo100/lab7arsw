b5bddfa4dfaeaf4566c844c7bce1b14c006e7f26
unknown <yorks.gomez@labinfo.is.escuelaing.edu.co>
2023-03-15 09:26:44 -0500

    arreglos

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,81 +1,5 @@
-<<<<<<< HEAD
-var apimock = apimock;
+var api = apimock;
 
-var app = (function (){
-    var author;
-    var blueprintName;
-
-    function getName() {
-            $("#name").text(author + "'s " + "blueprints:");
-        }
-
-     function getNameAuthorBlueprints() {
-        author = $("#author").val();
-        if (author === "") {
-            alert("Debe ingresar un nombre");
-        } else {
-            apimock.getBlueprintsByAuthor(author,authorData);
-        }
-     }
-
-     var authorData = function( data) {
-         $("#table tbody").empty();
-         if (data === undefined) {
-             alert("No existe el autor");
-             $("#name").empty();
-             $("#points").text("Total Points");
-             $("#nameblu").empty();
-         } else {
-             getName();
-             const datanew = data.map((elemento) => {
-                 return {
-                     name: elemento.name,
-                     puntos: elemento.points.length
-                 }
-             });
-
-             datanew.map((elementos) => {
-                 $("#table > tbody:last").append($("<tr><td>" + elementos.name + "</td><td>" + elementos.puntos.toString() +
-                     "</td><td>" + "<button  id=" + elementos.name + " onclick=app.getBlueprintByAuthorAndName(this)>open</button>" + "</td>"));
-             });
-
-             const totalPuntos = datanew.reduce((suma, {puntos}) => suma + puntos, 0);
-
-             $("#points").text("Total user points: " + totalPuntos);
-            }
-         }
-
-        function getBlueprintByAuthorAndName(data) {
-            author = $("#author").val();
-            blueprintName = data.id;
-            $("#nameblu").text("Current blueprint: " + blueprintName);
-            apimock.getBlueprintByAuthorAndName(author, blueprintName, printPoints);
-        }
-
-        function printPoints(data) {
-            const puntos = data.points;
-            var c = document.getElementById("myCanvas");
-            var ctx = c.getContext("2d");
-            ctx.clearRect(0, 0, c.width, c.height);
-            ctx.restore();
-            ctx.beginPath();
-            for (let i = 1; i < puntos.length; i++) {
-                ctx.moveTo(puntos[i - 1].x, puntos[i - 1].y);
-                ctx.lineTo(puntos[i].x, puntos[i].y);
-                if (i === puntos.length - 1) {
-                    ctx.moveTo(puntos[i].x, puntos[i].y);
-                    ctx.lineTo(puntos[0].x, puntos[0].y);
-                }
-            }
-            ctx.stroke();
-        }
-        
-    return{
-        getBlueprintByAuthorAndName:getBlueprintByAuthorAndName,
-        getNameAuthorBlueprints: getNameAuthorBlueprints
-    }
-})();
-=======
 var app = (function () {
     var author;
     var blueprintName;
@@ -89,13 +13,13 @@ var app = (function () {
         if (author === "") {
             alert("Debe ingresar un nombre");
         } else {
-            apiclient.getBlueprintsByAuthor(author, authorData);
+            api.getBlueprintsByAuthor(author, authorData);
         }
     }
 
     var authorData = function (data) {
         $("#table tbody").empty();
-        if (data === undefined) {
+        if (data == undefined) {
             alert("No existe el autor");
             $("#name").empty();
             $("#points").text("Total Points");
@@ -124,7 +48,7 @@ var app = (function () {
         author = $("#author").val();
         blueprintName = data.id;
         $("#nameblu").text("Current blueprint: " + blueprintName);
-        apiclient.getBlueprintByAuthorAndName(author, blueprintName, printPoints);
+        api.getBlueprintByAuthorAndName(author, blueprintName, printPoints);
     }
 
     function printPoints(data) {
@@ -150,7 +74,7 @@ var app = (function () {
         getNameAuthorBlueprints: getNameAuthorBlueprints,
         selectAuthor: (author) => {
             _selectedAuthor = author;
-            apiclient.getBlueprintsByAuthor(author, (blueprints) => _selectedBlueprints = blueprints);
+            api.getBlueprintsByAuthor(author, (blueprints) => _selectedBlueprints = blueprints);
         },
         getAuthor: () => {
             return _selectedAuthor;
@@ -160,7 +84,7 @@ var app = (function () {
         },
         draw: (blueprint) => {
 
-            apiclient.getBlueprintsByNameAndAuthor(author, blueprint, (found) => {
+            api.getBlueprintsByNameAndAuthor(author, blueprint, (found) => {
                 let canvas = document.getElementById("myCanvas");
                 let context = canvas.getContext('2d');
                 let points = found.points;
@@ -193,4 +117,3 @@ var app = (function () {
     };
 
     })();
->>>>>>> canvas/yorks
